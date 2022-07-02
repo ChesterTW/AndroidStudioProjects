432570625b9ca820ac90e5891c9d3ccdad0d6be5
Taro <smalltaro8759387@gmal.com>
2022-07-02 22:33:20 +0800

    Feat 自動產生單字，和各別的收藏鈕

--- basic_material/lib/main.dart
+++ basic_material/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,15 +22,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  /// 建構式 Construction：
+  /// 需給予參數 title
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  /// 宣告字串型別的 title，並在建構式中被定義。
   final String title;
 
+  /// 給予可變狀態
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _suggestions = <WordPair>[];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,8 +50,13 @@ class _MyHomePageState extends State<MyHomePage> {
               color: Colors.white,
             ),
             onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => FavoritePage()));
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const FavoritePage(
+                        //Data
+                        ),
+                  ));
             },
           ),
           const Padding(
@@ -52,7 +64,42 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-      body: const Center(),
+      body: _buildSuggestions(),
+    );
+  }
+
+  /// 產生單字
+  Widget _buildSuggestions() {
+    return ListView.builder(itemBuilder: (context, i) {
+      if (i.isOdd) return const Divider();
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return _buildRow(_suggestions[index]);
+    });
+  }
+
+  /// 產生單字列
+  Widget _buildRow(WordPair pair) {
+    bool _likeState = false;
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+      ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _likeState = !_likeState;
+              });
+            },
+            icon: Icon((_likeState) ? Icons.favorite : Icons.favorite_border),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -67,6 +114,34 @@ class FavoritePage extends StatelessWidget {
         title: const Text("收藏頁"),
         centerTitle: true,
       ),
+      body: const Center(
+        child: Text("Hello"),
+      ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, i) {
+          if (i.isOdd) return const Divider();
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return ListTile(title: Text("$_suggestions"));
+        });
   }
 }
