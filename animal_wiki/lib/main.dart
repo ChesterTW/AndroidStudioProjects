cd6355547126c80db7fd1e77e27df2c99b60eba3
Taro <smalltaro8759387@gmal.com>
2022-07-08 19:18:26 +0800

    去除多餘 Layout Widiget

--- animal_wiki/lib/main.dart
+++ animal_wiki/lib/main.dart
@@ -46,7 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget _buildBox() {
     return Container(
       width: double.infinity,
-      height: 560,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(25.0),
@@ -60,67 +59,57 @@ class _MyHomePageState extends State<MyHomePage> {
               borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
               image: DecorationImage(
                 image: AssetImage('images/BengalTiger.jpeg'),
-                fit: BoxFit.fitWidth,
+                fit: BoxFit.fill,
               ),
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const [
-                  IconButton(
-                    icon: Icon(Icons.description_outlined),
-                    iconSize: 30.0,
-                    onPressed: null,
-                  ),
-                  SizedBox(
-                    width: 5.0,
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.share_outlined),
-                    iconSize: 30.0,
-                    onPressed: null,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: const [
-                  IconButton(
-                    icon: Icon(Icons.bookmark_border_outlined),
-                    iconSize: 30.0,
-                    onPressed: null,
-                  )
-                ],
-              )
-            ],
-          ),
-          Column(
-            children: [
-              Container(
-                decoration: const BoxDecoration(color: Colors.white),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(15.0),
-                      child: Row(
-                        children: const [
-                          Text(
-                            "BengalTiger",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5, 7.5, 7.5, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    IconButton(
+                      icon: Icon(Icons.description_outlined),
+                      iconSize: 30.0,
+                      onPressed: null,
                     ),
-                    const Padding(
-                        padding: EdgeInsets.all(15.0),
-                        child: Text(
-                            "孟加拉虎（學名：Panthera tigris tigris）[1][2]又名印度虎，是目前數量最多，分布最廣的虎的亞種，1758年，孟加拉虎成為瑞典自然學家卡爾·林奈為老虎命名時的模式標本，因而也就成了指名亞種。孟加拉虎主要分布在印度和孟加拉國。孟加拉虎也是這兩個國家的珍稀動物。")),
+                    IconButton(
+                      icon: Icon(Icons.share_outlined),
+                      iconSize: 30.0,
+                      onPressed: null,
+                    ),
                   ],
                 ),
-              )
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    IconButton(
+                      icon: Icon(Icons.bookmark_border_outlined),
+                      iconSize: 30.0,
+                      onPressed: null,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Divider(),
+          Column(
+            children: const [
+              ListTile(
+                title: Text(
+                  "BengalTiger",
+                  style: TextStyle(fontSize: 24),
+                ),
+                subtitle: Text("學名：Panthera tigris tigris"),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text(
+                      "孟加拉虎（學名：Panthera tigris tigris）[1][2]又名印度虎，是目前數量最多，分布最廣的虎的亞種，1758年，孟加拉虎成為瑞典自然學家卡爾·林奈為老虎命名時的模式標本，因而也就成了指名亞種。孟加拉虎主要分布在印度和孟加拉國。孟加拉虎也是這兩個國家的珍稀動物。")),
             ],
           )
         ],
