09c64aeff0fd13ae9164013ce65e04fd04d8ffde
Taro <smalltaro8759387@gmal.com>
2022-07-10 20:35:16 +0800

    Feat Collection Page & Detail Page

--- animal_wiki/lib/main.dart
+++ animal_wiki/lib/main.dart
@@ -30,6 +30,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  /// 可以 Json 化，以簡潔 main.dart
+  Animal animalOtter = new Animal(
+      chName: "水獺",
+      enName: "Otter",
+      description:
+          "水獺是一類水棲、肉食性的哺乳動物，在動物分類學中屬於食肉目鼬科下的亞科級別，稱為水獺亞科（Lutrinae），現存七個屬及十三個物種。",
+      img: "images/otter.jpeg",
+      saved: false);
+  Animal animalBengalTiger = new Animal(
+      chName: "孟加拉虎",
+      enName: "BengalTiger",
+      description:
+          "孟加拉虎（學名：Panthera tigris tigris）[1][2]又名印度虎，是目前數量最多，分布最廣的虎的亞種，1758年，孟加拉虎成為瑞典自然學家卡爾·林奈為老虎命名時的模式標本，因而也就成了指名亞種。孟加拉虎主要分布在印度和孟加拉國。孟加拉虎也是這兩個國家的珍稀動物。",
+      img: "images/BengalTiger.jpeg",
+      saved: false);
+  Animal animalSlowpoke = new Animal(
+      chName: "呆呆獸",
+      enName: "Slowpoke",
+      description:
+          "呆呆獸是第一世代的寶可夢。 呆呆獸會用尾巴在水邊釣魚，根據設定，當大舌貝咬住呆呆獸的尾巴時，它就會進化成呆殼獸。 在動畫和旁支系列遊戲中存在這一情節，但在主系列遊戲中呆呆獸的進化與大舌貝無關。",
+      img: "images/Slowpoke.png",
+      saved: false);
+
+  final _saved = <Animal>{};
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,12 +81,9 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.grey[300],
         body: ListView(
           children: [
-            _buildCard("images/BengalTiger.jpeg", "孟加拉虎", "BengalTiger",
-                "孟加拉虎（學名：Panthera tigris tigris）[1][2]又名印度虎，是目前數量最多，分布最廣的虎的亞種，1758年，孟加拉虎成為瑞典自然學家卡爾·林奈為老虎命名時的模式標本，因而也就成了指名亞種。孟加拉虎主要分布在印度和孟加拉國。孟加拉虎也是這兩個國家的珍稀動物。"),
-            _buildCard("images/otter.jpeg", "水獺", "Otter",
-                "水獺是一類水棲、肉食性的哺乳動物，在動物分類學中屬於食肉目鼬科下的亞科級別，稱為水獺亞科（Lutrinae），現存七個屬及十三個物種。"),
-            _buildCard("images/Slowpoke.png", "呆呆獸", "Slowpoke",
-                "呆呆獸是第一世代的寶可夢。 呆呆獸會用尾巴在水邊釣魚，根據設定，當大舌貝咬住呆呆獸的尾巴時，它就會進化成呆殼獸。 在動畫和旁支系列遊戲中存在這一情節，但在主系列遊戲中呆呆獸的進化與大舌貝無關。"),
+            _buildCard(animalOtter),
+            _buildCard(animalBengalTiger),
+            _buildCard(animalSlowpoke)
           ],
         ));
   }
@@ -70,13 +92,37 @@ class _MyHomePageState extends State<MyHomePage> {
   void _enterBookmarks() {
     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
       return Scaffold(
-        appBar: AppBar(
-          title: const Text("Collection Page"),
-          centerTitle: false,
-        ),
-        body: const Text("Welcome to Collection Page"),
-      );
+          appBar: AppBar(
+            title: const Text("Collection Page"),
+            centerTitle: false,
+          ),
+          backgroundColor: Colors.grey[200],
+          body: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  mainAxisSpacing: 3.0,
+                  crossAxisSpacing: 3.0),
+              itemCount: _saved.length,
+              itemBuilder: (context, index) {
+                var temp = <Animal>[];
+                temp = _saved.toList();
+                return _previewCard(temp[index]);
+              }));
     }));
+  }
+
+  Widget _previewCard(Animal animal) {
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(),
+      child: InkWell(
+        child: Ink.image(
+          image: AssetImage(animal.img),
+          fit: BoxFit.fill,
+        ),
+      ),
+    );
   }
 
   /// buildCard，負責產生動物的「外頁」，
@@ -84,8 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
   /// 引數有四個，皆為 String 類型：imgPath, chName, enName, description，
   /// 當中使用到了一些方法：有 Container buildImage，和 Column buildContext, buildSection。
   /// 而本 Widget ，最終，會把各方法的回傳值組合起，並 return 為 Padding。
-  Widget _buildCard(
-      String imgPath, String chName, String enName, String description) {
+  Widget _buildCard(Animal animal) {
     //throw Exception();
     return Padding(
         // Padding : 設置左上右方向為 16.0，底部為 4.0，讓 _buildCard 與螢幕邊界擁有距離，
@@ -105,17 +150,17 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               /// 負責外頁的「圖片」
               /// Return ClipRRect, Child : Image
-              _buildImage(imgPath),
+              _buildImage(animal.img),
 
               /// 負責外頁的「按鈕列」
               /// Return Row, Children : 三個 buttonColumn
-              _buttonSection(),
+              _buttonSection(animal),
 
               /// 我是可愛的分隔線，負責將「按鈕列」、「文字」區分開來。
               const Divider(),
 
               /// 負責外頁的「中英文名稱」、「簡介」。
-              _buildContext(chName, enName, description)
+              _buildContext(animal)
             ],
           ),
         ));
@@ -142,16 +187,18 @@ class _MyHomePageState extends State<MyHomePage> {
   /// buildContext，負責創建外頁的「內容」，
   /// 接收三個 String 引數，分別為「中英文名稱」和「簡介」，
   /// 而最終會回傳成 Column。
-  Widget _buildContext(String chName, String enName, String description) {
+  Widget _buildContext(Animal animal) {
     return Column(
       children: [
         ListTile(
             title: Text(
-              chName,
+              animal.chName,
               style: const TextStyle(fontSize: 24),
             ),
-            subtitle: Text(enName)),
-        Padding(padding: const EdgeInsets.all(15.0), child: Text(description)),
+            subtitle: Text(animal.enName)),
+        Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(animal.description)),
       ],
     );
   }
@@ -187,17 +234,54 @@ class _MyHomePageState extends State<MyHomePage> {
   /// 呼叫多個 Column 方法 buildButtonColumn，以獲取「按鈕欄」，
   /// 並使用 MainAxisAlignment.spaceEvenly，將其平均分散，
   /// 並包在 Row 中 return。
-  Widget _buttonSection() {
+  Widget _buttonSection(Animal animal) {
     return Padding(
       padding: const EdgeInsets.only(top: 10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          _buildButtonColumn(Icons.description, '詳情'),
-          _buildButtonColumn(Icons.share, '分享'),
-          _buildButtonColumn(Icons.bookmark_border, '收藏'),
+          InkWell(
+            child: _buildButtonColumn(Icons.description, "詳情"),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return EnterDetailPage(animal: animal);
+              }));
+            },
+          ),
+          InkWell(
+            child: _buildButtonColumn(Icons.share, "分享"),
+            onTap: () {
+              setState(() {});
+            },
+          ),
+          InkWell(
+            child: _buildButtonColumn(
+                animal.saved ? Icons.bookmark : Icons.bookmark_border, "收藏"),
+            onTap: () {
+              setState(() {
+                animal.saved = !animal.saved;
+                animal.saved ? _saved.add(animal) : _saved.remove(animal);
+              });
+            },
+          )
         ],
       ),
+    );
+  }
+}
+
+class EnterDetailPage extends StatelessWidget {
+  /// 建構式 Construction：需給予 Animal 類型的變數
+  const EnterDetailPage({Key? key, required this.animal}) : super(key: key);
+
+  /// 宣告 Animal 型別的 animal，並在建構式中被定義。
+  final Animal animal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(animal.chName)),
+      body: Text(animal.description),
     );
   }
 }
@@ -226,7 +310,7 @@ class Animal {
   final String img;
 
   /// 宣告 bool 類型的 saved，負責儲存「是否存儲的狀態」。
-  final bool saved;
+  bool saved;
 
   /// 接收 Json 檔，將裡頭的數值定義給對應的「變數」。
   factory Animal.fromJson(Map<String, dynamic> json) => Animal(
